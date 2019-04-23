4d804d06cfeb4fa30905bfcfb19518a47683468d
daf <chris.daf@gmail.com>
2019-04-23 16:53:15 +0100

    exercise 2

---
+++ excerises/ex2.rb
@@ -0,0 +1,43 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create empty array
+  students = []
+  # get their first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add student hash to array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from user
+    name = gets.chomp
+  end
+  # return array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-----------------"
+end
+
+def print(students)
+  puts "Enter the first letter of the name you want to show"
+  letter = gets.chomp
+  students.each do |student|
+    if student[:name][0] == letter
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_footer(students)
+puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+# Nowt happens until we call the methods
+print_header
+print(students)
+print_footer(students)
